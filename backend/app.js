40392fa0df3f0557cc81c4314968672bfa9b622b
abodian <ajnbodian@gmail.com>
2023-02-28 14:01:37 +0000

    fetch request added

--- backend/app.js
+++ backend/app.js
@@ -2,3 +2,26 @@ const createError = require("http-errors");
 const express = require("express");
 const path = require("path");
 const logger = require("morgan");
+const app = express();
+const axios = require("axios");
+
+// setup for receiving JSON
+app.use(express.json());
+app.use(logger("dev"));
+app.use(express.json());
+app.use(express.static(path.join(__dirname, "public")));
+
+// middleware function to make TFL API requests
+
+async function getLineData() {
+  try {
+    const response = await axios.get(
+      "https://api.tfl.gov.uk/Line/victoria/Arrivals?app_id=a5a934e75afb4bc6815e8d5ec9cd0633&app_key=3f65cf90ca3d4e748dc28439c75e8686"
+    );
+    console.log(response);
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+getLineData();
