73dba267b2ee2f590202175c84e23936cd9f9886
abodian <ajnbodian@gmail.com>
2023-03-01 21:10:25 +0000

    split arrivaldata component away from app.js

--- frontend/src/components/app/App.js
+++ frontend/src/components/app/App.js
@@ -1,67 +1,15 @@
-import React, { useState, useEffect } from "react";
+import ArrivalData from "../arrivalData/ArrivalData";
+
 import "./App.css";
-import axios from "axios";
-const Tone = require('tone')
 
 function App() {
-  const [line, setLine] = useState("victoria");
-  const [data, setData] = useState(null);
-
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        console.log('FETCHING API DATA')
-        const response = await axios.get(`http://localhost:8080/line/${line}`);
-        const data = await response.data.transformedData
-        setData(data);
-        
-      } catch (error) {
-        console.error(error);
-      }
-    };
-
-    fetchData();
-    
-    const intervalId = setInterval(() => {
-      fetchData();
-    }, 30000);
-
-    return () => clearInterval(intervalId);
-  }, [line]);
-
-  useEffect(() => {
-    const checkExpectedArrival = () => {
-      console.log('CHECK FOR TIME MATCH')
-      if (data) {
-        const now = new Date();
-        data.forEach((obj) => {
-          const [hours, minutes, seconds] = obj.expectedArrival.split(':');
-          const expectedArrival = new Date();
-          expectedArrival.setHours(hours);
-          expectedArrival.setMinutes(minutes);
-          expectedArrival.setSeconds(seconds);
-          if (expectedArrival.getTime() === now.getTime()) {
-            console.log(obj);
-            const keyNotes = ['C4', 'D4', 'E4', 'F4', 'G4', 'A4', 'B4'];
-            const noteIndex = Math.floor(Math.random() * keyNotes.length);
-            const synth = new Tone.Synth().toDestination();
-            synth.triggerAttackRelease("C4", "8n")
-          }
-        });
-      }
-    };
-
-    const intervalId = setInterval(() => {
-      checkExpectedArrival();
-    }, 1000);
-
-    return () => clearInterval(intervalId);
-  }, [data]);
-  
   return (
     <div className="App">
       <h1>Welcome!</h1>
-      {console.log(data)}
+      <ArrivalData line="victoria" noteLength="4n" />
+      <ArrivalData line="central" noteLength="2n" />
+      <ArrivalData line="jubilee" noteLength="8n" />
+      <ArrivalData line="northern" noteLength ="1m" />
     </div>
   );
 }
