99b224ad4c4fde93aadea93a6f88edfc4af87967
Abbas Alam <abbas_alam@hotmail.co.uk>
2023-03-02 11:01:33 +0000

    css pulse

--- frontend/src/components/app/App.js
+++ frontend/src/components/app/App.js
@@ -23,9 +23,22 @@ function App() {
   return (
     <div className="App">
       <h1>Welcome!</h1>
+      <div class="circle"></div> 
+      <div class="center">
+      <div class="circle pulse orange"></div>
+      <div class="circle pulse blue"></div>
+      <div class="circle pulse rose"></div>
+      <div class="circle pulse green"></div>
+      <div class="circle2"></div>
+
+</div>
+
       {console.log(data)}
     </div>
+    
   );
 }
 
 export default App;
+
+
