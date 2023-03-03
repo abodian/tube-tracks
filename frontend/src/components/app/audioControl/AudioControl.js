d086e81a0e52bad195fb6d8d34ccc54b93ab44d1
terrybro66 <terrybro61@gmail.com>
2023-03-03 14:00:14 +0000

    fixing audio pause

--- frontend/src/components/app/audioControl/AudioControl.js
+++ frontend/src/components/app/audioControl/AudioControl.js
@@ -11,14 +11,14 @@ const PlayStop = () => {
         <img
           src="/images/play.png"
           alt="play button"
-          onClick={Tone.start()}
+          onClick={Tone.start}
         ></img>
       </div>
       <div className={styles.playnpause}>
         <img
           src="/images/stop.png"
           alt="pause button"
-          onClick={Tone.pause()}
+          onClick={Tone.Transport.pause}
         ></img>
       </div>
       <div className={styles.instruct}>
