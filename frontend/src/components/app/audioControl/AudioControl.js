a9d9e30810ff33da92d78ea9295222025c87c9a2
terrybro66 <terrybro61@gmail.com>
2023-03-03 14:17:53 +0000

    fix problem

--- frontend/src/components/app/audioControl/AudioControl.js
+++ frontend/src/components/app/audioControl/AudioControl.js
@@ -1,10 +1,16 @@
-import React from "react";
+import React, { useState } from "react";
 
 import styles from "./AudioControl.module.css";
 
 const Tone = require("tone");
 
 const PlayStop = () => {
+  const [volume, setVolume] = useState(50);
+
+  const handleVolumeChange = (event) => {
+    setVolume(event.target.value);
+  };
+
   return (
     <div className={styles.container}>
       <div className={styles.playnpause}>
@@ -24,8 +30,20 @@ const PlayStop = () => {
       <div className={styles.instruct}>
         Turn up your speakers, relax and press play!
       </div>
-      <div className={styles.speaker}>
-        <img src="/images/speaker.png" alt="adjust volume"></img>
+      <div class={styles.volume}>
+        <div className={styles.speaker}>
+          <img src="/images/speaker.png" alt="adjust volume"></img>
+        </div>
+        <div className={styles.slider}>
+          <input
+            type="range"
+            min="0"
+            max="100"
+            value={volume}
+            onChange={handleVolumeChange}
+          />
+          <span>{volume}%</span>
+        </div>
       </div>
     </div>
   );
