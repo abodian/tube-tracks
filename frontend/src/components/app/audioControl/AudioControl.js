09cce2d802746ab22bc94a08aec0723c70b996aa
terrybro66 <terrybro61@gmail.com>
2023-03-03 13:28:40 +0000

    corrected html

--- frontend/src/components/app/audioControl/AudioControl.js
+++ frontend/src/components/app/audioControl/AudioControl.js
@@ -2,14 +2,24 @@ import React from "react";
 
 import styles from "./AudioControl.module.css";
 
+const Tone = require("tone");
+
 const PlayStop = () => {
   return (
     <div className={styles.container}>
       <div className={styles.playnpause}>
-        <img src="/images/play.png" alt="play button"></img>
+        <img
+          src="/images/play.png"
+          alt="play button"
+          onClick={Tone.start()}
+        ></img>
       </div>
       <div className={styles.playnpause}>
-        <img src="/images/stop.png" alt="pause button"></img>
+        <img
+          src="/images/stop.png"
+          alt="pause button"
+          onClick={Tone.pause()}
+        ></img>
       </div>
       <div className={styles.instruct}>
         Turn up your speakers, relax and press play!
