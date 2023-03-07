5c722321e2349845f12ee9e0e8a3518ace354edb
abodian <ajnbodian@gmail.com>
2023-03-07 18:40:17 +0000

    feature switches labeled but non-functional

--- frontend/src/components/features/Features.js
+++ frontend/src/components/features/Features.js
@@ -1,21 +1,29 @@
 function Features () {
   return (
     <div>
+      <b>Ambience</b>
       <div>
-        <input id="Victoria" type="checkbox"></input>
-        <label>Happy</label>
+        <br></br>
+        <input type="checkbox" id="bg-one" className="feature-switch" checked></input>
+        <label for="bg-one">Orchestron</label>
       </div>
       <div>
-        <input type="checkbox" name="piccadilly"></input>
-        <label>Sad</label>
+        <input id="bg-two" type="checkbox" className="feature-switch"></input>
+        <label for="bg-two">Cosmic Waves</label>
+      </div>
+      <b>Instruments</b>
+      <div>
+        <br></br>
+        <input id="piano" type="checkbox" className="feature-switch" checked></input>
+        <label for="piano">Piano</label>
       </div>
       <div>
-        <input id="Victoria" type="checkbox"></input>
-        <label>Single Notes</label>
+        <input id="pizzicato-strings" type="checkbox" className="feature-switch" checked></input>
+        <label for="pizzicato-strings">Pizzicato Strings</label>
       </div>
       <div>
-        <input type="checkbox" name="piccadilly"></input>
-        <label>Chords</label>
+        <input id="marimba" type="checkbox" className="feature-switch" checked></input>
+        <label for="marimba">Marimba</label>
       </div>
   </div>
   )
