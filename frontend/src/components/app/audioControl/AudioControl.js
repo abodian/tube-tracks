c6574466a3e113c8e3b85cee2d570d7619defce6
terrybro66 <terrybro61@gmail.com>
2023-03-07 06:59:43 +0000

    fixed stuff

--- frontend/src/components/app/audioControl/AudioControl.js
+++ frontend/src/components/app/audioControl/AudioControl.js
@@ -10,35 +10,34 @@ const PlayStop = ({ stop, start }) => {
   const handleVolumeChange = (event) => {
     setVolume(event.target.value);
   };
+
   return (
-    <>
-      <div className={styles.container}>
-        <div className={styles.playnpause}>
-          <img src="/images/play.png" alt="play button" onClick={start}></img>
-        </div>
-        <div className={styles.playnpause}>
-          <img src="/images/stop.png" alt="pause button" onClick={stop}></img>
-        </div>
-        <div className={styles.instruct}>
-          Turn up your speakers, relax and press play!
-        </div>
-        <div class={styles.volume}>
-          <div className={styles.speaker}>
-            <img src="/images/speaker.png" alt="adjust volume"></img>
-          </div>
-          <div className={styles.jumbo}>
-            <input
-              type="range"
-              min="0"
-              max="100"
-              value={volume}
-              onChange={handleVolumeChange}
-            />
-            <span>{volume}%</span>
-          </div>
-        </div>
+    <div className={styles.container}>
+      <div className={styles.playnpause}>
+        <img src="/images/play.png" alt="play button" onClick={start}></img>
       </div>
-    </>
+      <div className={styles.playnpause}>
+        <img src="/images/stop.png" alt="pause button" onClick={stop}></img>
+      </div>
+      <div className={styles.instruct}>
+        Turn up your speakers, relax and press play!
+      </div>
+      <div class={styles.volume}>
+        <div className={styles.speaker}>
+          <img src="/images/speaker.png" alt="adjust volume"></img>
+        </div>
+        <div className={styles.slider}>
+          <input
+            type="range"
+            min="0"
+            max="100"
+            value={volume}
+            onChange={handleVolumeChange}
+          />
+        </div>
+        <div> {volume}%</div>
+      </div>
+    </div>
   );
 };
 
