94ce62cdac44092c1ae0450ed9b1d05adb726a4b
abodian <ajnbodian@gmail.com>
2023-03-09 17:30:28 +0000

    cors header

--- backend/app.js
+++ backend/app.js
@@ -8,6 +8,15 @@ const app = express();
 
 app.use(cors());
 
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "https://tube-tracks.onrender.com"); // update to match the domain you will make the request from
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
+
 app.get("/products/:id", function (req, res, next) {
   res.json({ msg: "This is CORS-enabled for all origins!" });
 });
