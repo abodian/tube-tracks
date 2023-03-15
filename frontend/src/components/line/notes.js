ea00bd10f115c9bbf7fc55502c810d2eddbd5450
terrybro66 <terrybro61@gmail.com>
2023-03-15 15:11:33 +0000

    moving stations and instruments into own files

---
+++ frontend/src/components/line/notes.js
@@ -0,0 +1,52 @@
+const notes = {
+  piano: {
+    CS3Piano:
+      "https://res.cloudinary.com/did9lgedz/video/upload/v1678117938/tube-tracks/C_3_1_z6dqvg.wav",
+    DS3Piano:
+      "https://res.cloudinary.com/did9lgedz/video/upload/v1678117938/tube-tracks/D_3_1_aghaaz.wav",
+    FS3Piano:
+      "https://res.cloudinary.com/did9lgedz/video/upload/v1678117939/tube-tracks/F_3_1_xienr0.wav",
+    GS3Piano:
+      "https://res.cloudinary.com/did9lgedz/video/upload/v1678117939/tube-tracks/G_3_1_atwwuv.wav",
+    AS3Piano:
+      "https://res.cloudinary.com/did9lgedz/video/upload/v1678117938/tube-tracks/A_3_1_o0knhr.wav",
+  },
+  strings: {
+    CS2Pizz:
+      "https://res.cloudinary.com/did9lgedz/video/upload/v1678384968/tube-tracks/improved%20pizz/CS2_-_Pizz_1_fgtvx6.wav",
+    DS2Pizz:
+      "https://res.cloudinary.com/did9lgedz/video/upload/v1678384967/tube-tracks/improved%20pizz/DS2_-_Pizz_1_icmwmk.wav",
+    FS2Pizz:
+      "https://res.cloudinary.com/did9lgedz/video/upload/v1678384967/tube-tracks/improved%20pizz/FS2_-_Pizz_1_msynye.wav",
+    GS2Pizz:
+      "https://res.cloudinary.com/did9lgedz/video/upload/v1678384967/tube-tracks/improved%20pizz/GS2_-_Pizz_1_uzw6fq.wav",
+    AS2Pizz:
+      "https://res.cloudinary.com/did9lgedz/video/upload/v1678384966/tube-tracks/improved%20pizz/AS2_-_Pizz_1_ksvht7.wav",
+  },
+  marimba: {
+    FSCSChMarimba:
+      "https://res.cloudinary.com/did9lgedz/video/upload/v1678387812/tube-tracks/improved%20marimba/FSCSChMarimba1_1_omqkp1.wav",
+    GSDSChMarimba:
+      "https://res.cloudinary.com/did9lgedz/video/upload/v1678387812/tube-tracks/improved%20marimba/GSDSChMarimba1_1_lnbbwi.wav",
+    DSCSClusterMarimba:
+      "https://res.cloudinary.com/did9lgedz/video/upload/v1678387812/tube-tracks/improved%20marimba/DSCShMarimba1_1_timwwe.wav",
+  },
+  bass: {
+    EBass:
+      "https://res.cloudinary.com/did9lgedz/video/upload/v1678189276/tube-tracks/Bass%20Samples/E_Bass_bip_1_qzgqbn.wav",
+    DSBass:
+      "https://res.cloudinary.com/did9lgedz/video/upload/v1678189276/tube-tracks/Bass%20Samples/ES_Bass_bip_1_qj6wgi.wav",
+    FSBass:
+      "https://res.cloudinary.com/did9lgedz/video/upload/v1678189276/tube-tracks/Bass%20Samples/FS_Bass_bip_1_j6rwcm.wav",
+    GSBass:
+      "https://res.cloudinary.com/did9lgedz/video/upload/v1678189276/tube-tracks/Bass%20Samples/GS_Bass_bip_1_zam6i0.wav",
+    ASBass:
+      "https://res.cloudinary.com/did9lgedz/video/upload/v1678190334/tube-tracks/Bass%20Samples/AS_Bass_bip_1_etl9hi.wav",
+    BBass:
+      "https://res.cloudinary.com/did9lgedz/video/upload/v1678189276/tube-tracks/Bass%20Samples/B_Bass_bip_1_si5hac.wav",
+    CSBass:
+      "https://res.cloudinary.com/did9lgedz/video/upload/v1678189276/tube-tracks/Bass%20Samples/C_Bass_bip_1_dhhlfd.wav",
+  },
+};
+
+export default notes;
